6dd126c0f4da2e6e452746d7cfd5f66f43ab7556
micha <mmdanziger@gmail.com>
2020-05-31 15:05:14 -0400

    added external ocean value input

--- mobility_cartogram.py
+++ mobility_cartogram.py
@@ -59,27 +59,37 @@ if __name__ == "__main__":
     mobility_data_fname = "/home/micha/AggData/20200310.csv" if len(argv) < 2 else argv[1]
     polygon_data_fname = "/home/micha/devsnaps/cart-1.2.2/nyc_bg.geojson"
     polygons_to_scale_fname = "/home/micha/nyc_census_tracts.geojson"
-    cartogram_polygon_fname = "nyc_tag.geojson" if len(argv) < 3 else argv[2]
-    freq = "1H" if len(argv) < 4 else argv[3]
+    cartogram_polygon_fname = "nyc_tag.geojson" 
+    freq = "1H" 
+    external_mean_val = None if len(argv) < 3 else float(argv[2])
+    external_mean_val_weight = 0 if len(argv) < 4 else float(argv[3])
     dfad = pd.read_csv(mobility_data_fname,dtype={"bg":str,"id_count":float},parse_dates=["time"])
     dfadp = dfad.pivot("time","bg")
     dfadp = dfadp.resample(freq).interpolate().fillna(0)
     for t,d in dfadp.iterrows():
         mc = Cartogram()
+        mc.imsize = (800,1000)
+        mc.pad_width = (300,300)
         timestring = t.isoformat()
-        cartogram_polygon_fname = f"mobility_cartogram_{timestring}_{freq}.geojson"
+        cartogram_polygon_fname = f"mobility_cartogram_{timestring}_{freq}-{external_mean_val}-{external_mean_val_weight}.geojson"
         data_df = pd.DataFrame(d.droplevel(level=0)).rename(columns=lambda x: mc.data_key)
         log_step("Loading data")
         mc.load_polygon_df(polygon_data_fname,"GEOID")
         mc.polygon_df = mc.polygon_df[mc.polygon_df.COUNTYFP == "061"]
         mc.polygon_df,mc.mean_val = custom_join(mc.polygon_df,data_df)
+        if external_mean_val is not None:
+            print("Found external_mean_val %.12f and weight %.2f"%(external_mean_val,external_mean_val_weight))
+            original_mean_val = mc.mean_val
+            mc.mean_val = external_mean_val*external_mean_val_weight + mc.mean_val*(1 - external_mean_val_weight)
+            mc.polygon_df.loc[(mc.polygon_df.ALAND <= 0, mc.data_key)] = mc.mean_val
+            print("Using mean val %.12f instead of %.12f"%(mc.mean_val,original_mean_val))
         log_step("Preparing data for cart")
         mc.create_geocube_density_map()
         log_step("Running cart")
         mc.run_cart_on_density_map()
         log_step("Loading cart results")
         mc.load_interpolated_output()
-        plt.figure(figsize=tuple(reversed(np.round(12*np.array(mc.padded_im.shape).T/max(mc.padded_im.shape)))))
+        plt.figure(figsize=tuple(reversed(np.round(16*np.array(mc.padded_im.shape).T/max(mc.padded_im.shape)))))
         mc.plot_density_map()
         plt.title(timestring)
         plt.tight_layout()
