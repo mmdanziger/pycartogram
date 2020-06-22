655b9fe577e8d8166940c0afe920dbad4c26ed64
micha <mmdanziger@gmail.com>
2020-06-21 22:34:38 -0400

    added function to plot geojsons

---
+++ plot_geojson.py
@@ -0,0 +1,39 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from sys import argv
+from os.path import basename,join
+import dateutil
+from multiprocessing import Pool
+from glob import glob
+
+working_dir = argv[1]
+globstring = argv[2]
+plt.switch_backend("Agg")
+fig_w,fig_h = (3,10)
+
+
+def plot_geojson(fname):
+    gdf = gpd.read_file(fname).to_crs( "EPSG:6539")
+    try:
+        origin=gdf.unary_union.centroid
+    except:
+        gdf= gdf.set_geometry(gdf.geometry.apply(lambda x: x.buffer(0)))
+        origin=gdf.unary_union.centroid
+    gdf = gdf.set_geometry(gdf.rotate(28.5, origin=origin))
+
+    f, a = plt.subplots(figsize=(fig_w,fig_h),frameon=False)
+    ts=basename(fname).split("_")[-2]
+    print(ts)
+    dt = dateutil.parser.parse(ts)
+
+    gdf.plot(ax=a,facecolor="none",edgecolor="black",linewidth=1)
+    a.text(0.1, 0.99, dt.strftime("%a, %b %d, %I%p"), bbox=dict(facecolor='black', alpha=0.9),transform=a.transAxes,color="darkgray",fontdict={"size":10})            
+            
+    a.axis("off")
+    ofname=".".join(basename(fname).split(".")[:-1])+".png"
+    plt.savefig(ofname,dpi=300)
+    return 1
+
+with Pool() as pool:
+    res = pool.map_async(plot_geojson,sorted(glob(join(working_dir,globstring))))
+    print(sum(res.get()))
